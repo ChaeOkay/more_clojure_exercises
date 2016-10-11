e8d72bcb56a17e3ec1032e605b3aa226b85d2568
chaeokay <chae@healthfinch.com>
2016-10-10 21:23:21 -0400

    Cleans up wording

--- eulers/src/eulers/core.clj
+++ eulers/src/eulers/core.clj
@@ -14,25 +14,28 @@
        (re-seq #"\w+")
        sort))
 
-(defn name-collection [name]
+(defn name-letters [name]
   "Returns a StringSeq containing lower case chars in a name."
   (seq (str/lower-case name)))
 
 (defn index-plus-one [item collection]
+  "Adds one to the index of a given item within a collection."
   (+ 1 (.indexOf collection item)))
 
 (defn name-weight [name]
   "Returns a Long representing the sum of each weighted char letter in a name."
-  (loop [letters (name-collection name)
+  (loop [letters (name-letters name)
          acc 0]
     (if (empty? letters)
       acc
       (recur (rest letters) (+ acc (index-plus-one (first letters) alphabet))))))
 
 (defn name-score [name]
+  "Returns a Long that is the name index multiplied by its weight."
   (* (index-plus-one name name-list) (name-weight name)))
 
 (def total-score
+  "Returns the sum of all name-scores."
   (loop [names' names
          acc 0]
     (if (empty? names')
